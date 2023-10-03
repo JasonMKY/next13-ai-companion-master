061be2cde606de8aaa27f89b7222160e7d0505d1
Jason McKay <jmck38845@gmail.com>
2023-10-03 19:21:40 -0400

    .

--- app/(root)/(routes)/privacy/page.tsx
+++ app/(root)/(routes)/privacy/page.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 
 const PrivacyPolicy = () => {
-  return (
-    <div>
-      <h1>Privacy Policy</h1>
-      <p>
-        Last updated: October 03, 2023
-        This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.
-        We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>
+                Last updated: October 03, 2023
+                This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.
+                We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.
 
-        Interpretation
-        The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.
-        
-        
-      </p>
-    </div>
-  );
+                Interpretation
+                The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.
+
+                Definitions
+                For the purposes of this Privacy Policy:
+                Account - means a unique account created for You to access our Service or parts of our Service.
+                Affiliate - means an entity that controls, is controlled by or is under common control with a party, where "control" means ownership of 50% or more of the shares, equity interest or other securities entitled to vote for election of directors or other managing authority.
+                Company - (referred to as either "the Company", "We", "Us" or "Our" in this Agreement) refers to RoleplayPals.
+                <strong>Cookies</strong> are small files that are placed on Your computer, mobile device or any other device by a website, containing the details of Your browsing history on that website among its many uses.
+
+
+            </p>
+        </div>
+    );
 };
 
 export default PrivacyPolicy;
