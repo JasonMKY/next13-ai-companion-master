c8472ca6134b81dbd1380dede875258d46eab80a
Jason McKay <jmck38845@gmail.com>
2023-10-03 19:07:55 -0400

    .

--- app/(root)/(routes)/privacy/page.tsx
+++ app/(root)/(routes)/privacy/page.tsx
@@ -4,22 +4,11 @@ const PrivacyPolicy = () => {
   return (
     <div>
       <h1>Privacy Policy</h1>
-      <p>Last updated: October 03, 2023</p>
       <p>
-        This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information
-        when You use the Service and tells You about Your privacy rights and how the law protects You.
+        Last updated: October 03, 2023
+        This is where you can provide your privacy policy content. Make sure to include all the necessary information
+        regarding data collection, usage, and protection.
       </p>
-      <p>
-        We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and
-        use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the{' '}
-        <a href="https://www.freeprivacypolicy.com/free-privacy-policy-generator/" target="_blank" rel="noopener noreferrer">Free Privacy Policy Generator</a>.
-      </p>
-      {/* ... Other content ... */}
-      <h2>Contact Us</h2>
-      <p>If you have any questions about this Privacy Policy, You can contact us:</p>
-      <ul>
-        <li>By Email: jmck38845@gmail.com</li>
-      </ul>
     </div>
   );
 };
