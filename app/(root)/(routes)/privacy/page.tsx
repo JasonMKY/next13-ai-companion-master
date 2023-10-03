b6dd8f90fee233f62fb90f7185bbfda31ee70476
Jason McKay <jmck38845@gmail.com>
2023-10-03 19:12:46 -0400

    tr

--- app/(root)/(routes)/privacy/page.tsx
+++ app/(root)/(routes)/privacy/page.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 const PrivacyPolicy = () => {
   return (
     <div>
-      <h3>Privacy Policy</h3>
-      <p>Test</p>
+      <h1>Privacy Policy</h1>
       <p>
         Last updated: October 03, 2023
-        This is where you can provide your privacy policy content. Make sure to include all the necessary information
-        regarding data collection, usage, and protection.
-        787
+        This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.
+        We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.
+
+        Interpretation
+        The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.
+        
+        
       </p>
     </div>
   );
