cdd89982023f535d199e6559e918d63bb5d1fe0d
Jason McKay <jmck38845@gmail.com>
2023-10-03 18:21:18 -0400

    b

--- app/(root)/(routes)/privacy/page.tsx
+++ app/(root)/(routes)/privacy/page.tsx
@@ -1,1 +1,15 @@
-{"testing"}
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+        This is where you can provide your privacy policy content. Make sure to include all the necessary information
+        regarding data collection, usage, and protection.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
