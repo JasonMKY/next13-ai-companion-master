f0df782d33320d166c3e5e60cb8381e5c4f2ee20
Jason McKay <jmck38845@gmail.com>
2023-10-05 21:09:12 -0400

    u

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,7 +43,7 @@ function Navbar({ isPro, companionId, userId }: NavbarProps) {
               font.className
             )}
           >
-            roleplaypal.fun
+            roleplaypals.fun
           </h1>
         </Link>
       </div>
