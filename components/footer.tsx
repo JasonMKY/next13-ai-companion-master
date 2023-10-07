261f3a8549bcbb581503fe6f8aa6f24f268fe8ea
Jason McKay <jmck38845@gmail.com>
2023-10-06 21:35:42 -0400

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,30 +1,14 @@
 import React from "react";
-import { Menu, Sparkles } from "lucide-react";
-import { Poppins } from "next/font/google";
-import Link from "next/link";
-import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "./mode-toggle";
-import MobileSidebar from "./MobileSidebar";
-import { useProModal } from "@/hooks/use-pro-modal";
-import { redirect, useRouter } from "next/navigation";
-import { Router } from "next/router";
 
 const Footer = () => {
+  const style = {
+    marginLeft: "0.6in", // Set the left margin to 3 inches
+  };
+  
   return (
-    <footer className="fixed max-w-screen-2xl w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
-      <div className="flex items-center">
+    <footer className="fixed bottom-0 w-full bg-background text-foreground">
+      <div className="container mx-auto py-4">
         <ul className="flex justify-center space-x-6">
-          <h1
-        className={cn(
-              "hidden md:block text-xl md:text-3xl font-bold text-primary"
-      
-            )}
-          >
-            
-            </h1>
           <li>
             <a href="/privacy-policy" className="text-gray-600 hover:text-gray-900">
               Privacy Policy
@@ -38,27 +22,6 @@ const Footer = () => {
         </ul>
       </div>
     </footer>
-
-    /* 
-    <div className="fixed max-w-screen-2xl w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
-      <div className="flex items-center">
-        <MobileSidebar
-          isPro={isPro}
-          companionId={companionId}
-          userId={userId}
-        />
-        <Link href="/">
-          <h1
-            className={cn(
-              "hidden md:block text-xl md:text-3xl font-bold text-primary",
-              font.className
-            )}
-          >
-            roleplaypals.fun
-          </h1>
-        </Link>
-      </div>
-    */
   );
 };
 
