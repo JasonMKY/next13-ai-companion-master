c5291bbd81ae34152a712abd581c69185345cc80
Jason McKay <jmck38845@gmail.com>
2023-10-06 21:10:10 -0400

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-background text-foreground">
-      <div className="fixed max-w-screen-2xl w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
+    <footer className="fixed bottom-0 w-full bg-background text-foreground py-6">
+      <div className="container mx-auto">
         <ul className="flex justify-center space-x-6">
           <li>
             <a href="/privacy-policy" className="text-gray-600 hover:text-gray-900">
