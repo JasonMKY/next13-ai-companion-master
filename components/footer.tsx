2a5a41caf5b0bf30f650e1254ef5df3e8c11121c
Jason McKay <jmck38845@gmail.com>
2023-10-06 21:50:37 -0400

    update 7

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Footer = () => {
   const style = {
-    marginLeft: "5in", // Set the left margin to 3 inches
+    marginLeft: "0.6in", // Set the left margin to 3 inches
   };
 
   return (
     <footer className="fixed bottom-0 w-full bg-background text-foreground" style={style}>
-      <div className="container mx-auto py-10">
+      <div className="container mx-auto py-4">
         <ul className="flex justify-center space-x-6">
           <li>
             <a href="/privacy-policy" className="text-gray-600 hover:text-gray-900">
-              Privay Policy
+              Privacy Policy
             </a>
           </li>
           <li>
